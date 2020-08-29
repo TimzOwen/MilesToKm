3d498d94786a66dd9b0eff334cecb794036563c9
TimzOwen <ktimothy@kabarak.ac.ke>
2020-08-29 05:57:20 +0300

    updated view code and assignment

--- app/src/main/java/com/owen/milestokm/MainActivity.java
+++ app/src/main/java/com/owen/milestokm/MainActivity.java
@@ -3,6 +3,11 @@ package com.owen.milestokm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the views using IDs
+        Button btnKm2Miles = findViewById(R.id.btnKmToMiles);
+        Button btnMils2Km = findViewById(R.id.btnMilesToKm);
+
+
+        // Setting onClick listeners to respond to button clicks
+        btnKm2Miles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get the edit text input and convert them to string values
+                final EditText etMilesToKm = findViewById(R.id.etMilesToKm);
+                final EditText etKmToMiles = findViewById(R.id.etKmToMiles);
+
+                //convert the vlues in double from the string inputs
+                double vMiles = Double.parseDouble(etKmToMiles.getText().toString());
+                double vKm = vMiles / 0.62137;
+
+                //format the value in decimal
+                DecimalFormat format = new DecimalFormat("##.##");
+                etMilesToKm.setText(format.format(vKm));
+
+
+            }
+        });
+        btnMils2Km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get the IDs
+                final EditText etKmToMiles = findViewById(R.id.etKmToMiles);
+                final EditText etMilesToKm = findViewById(R.id.etMilesToKm);
+
+                //do the converstion/
+                // Assgined task
+
+            }
+        });
     }
 }
