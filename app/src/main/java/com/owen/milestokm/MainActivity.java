93fded5ad7f13c6e753ffa52390d99ea4d579e13
TimzOwen <ktimothy@kabarak.ac.ke>
2020-08-30 17:38:17 +0300

    updated user name sunday biweekly intro to andorid

--- app/src/main/java/com/owen/milestokm/MainActivity.java
+++ app/src/main/java/com/owen/milestokm/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -16,10 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText userName = findViewById(R.id.etUserName);
+
+
         //Find the views using IDs
         Button btnKm2Miles = findViewById(R.id.btnKmToMiles);
         Button btnMils2Km = findViewById(R.id.btnMilesToKm);
-
+        
 
         // Setting onClick listeners to respond to button clicks
         btnKm2Miles.setOnClickListener(new View.OnClickListener() {
@@ -28,17 +32,15 @@ public class MainActivity extends AppCompatActivity {
                 //get the edit text input and convert them to string values
                 final EditText etMilesToKm = findViewById(R.id.etMilesToKm);
                 final EditText etKmToMiles = findViewById(R.id.etKmToMiles);
-
                 double vMiles = Double.valueOf(etKmToMiles.getText().toString());
-
                 //convert the vlues in double from the string inputs
-
                 double vKm = vMiles / 0.62137;
-
                 //format the value in decimal
                 DecimalFormat format = new DecimalFormat("##.##");
                 etMilesToKm.setText(format.format(vKm));
+                Toast.makeText(MainActivity.this, "n_o_n_e", Toast.LENGTH_SHORT).show();
 
+                userName.setText(userName.getText().toString());
 
             }
         });
@@ -48,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 //get the IDs
                 final EditText etKmToMiles = findViewById(R.id.etKmToMiles);
                 final EditText etMilesToKm = findViewById(R.id.etMilesToKm);
-
                 //do the converstion/
                 // Assgined task
                 double vMiles = Double.valueOf(etMilesToKm.getText().toString());
@@ -59,5 +60,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
